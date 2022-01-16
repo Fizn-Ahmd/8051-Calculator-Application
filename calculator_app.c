7b0c79a9618cc0bc3a617836368266dfda8aab19
Fizn-Ahmd <faizanahmedfzn@gmail.com>
2022-01-16 19:47:25 +0530

    Add clang-format

--- calculator_app.c
+++ calculator_app.c
@@ -3,14 +3,15 @@
 #define MAX_DIGITS_TO_CALCULATE 10
 
 s16 atoi(s8 *);
-void rearrange_char_arr(s8 *, s16 );
-void rearrange_int_arr(s16 *, s16 );
+void rearrange_char_arr(s8 *, s16);
+void rearrange_int_arr(s16 *, s16);
 
 void main()
 {
 	s16 i = 0, j = 0, k = 0;
 	s16 converted_int_arr[MAX_DIGITS_TO_CALCULATE];
-	s8 unconverted_char_arr[MAX_DIGITS_TO_CALCULATE], sign_arr[MAX_DIGITS_TO_CALCULATE];
+	s8 unconverted_char_arr[MAX_DIGITS_TO_CALCULATE];
+	s8 sign_arr[MAX_DIGITS_TO_CALCULATE];
 	s8 click;
 	lcd_init();
 	while (1) {
@@ -24,7 +25,8 @@ void main()
 		}
 		lcd_input(click);
 
-		if (click == '+' || click == '*' || click == '/' || click == '-') {
+		if (click == '+' || click == '*' || click == '/' ||
+		    click == '-') {
 			unconverted_char_arr[i] = '\0';
 			i = 0;
 			sign_arr[k] = click;
@@ -39,29 +41,33 @@ void main()
 	sign_arr[k] = '\0';
 	for (i = 0; sign_arr[i]; i++) {
 		if (sign_arr[i] == '*') {
-			converted_int_arr[i] = converted_int_arr[i] * converted_int_arr[i+1];
+			converted_int_arr[i] =
+			    converted_int_arr[i] * converted_int_arr[i + 1];
 			rearrange_char_arr(sign_arr, i);
-			rearrange_int_arr(converted_int_arr, i+1);
+			rearrange_int_arr(converted_int_arr, i + 1);
 			i--;
 		}
 		if (sign_arr[i] == '/') {
-			converted_int_arr[i] = converted_int_arr[i] / converted_int_arr[i+1];
+			converted_int_arr[i] =
+			    converted_int_arr[i] / converted_int_arr[i + 1];
 			rearrange_char_arr(sign_arr, i);
-			rearrange_int_arr(converted_int_arr, i+1);
+			rearrange_int_arr(converted_int_arr, i + 1);
 			i--;
 		}
 	}
-	for (i=0; sign_arr[i]; i++) {
+	for (i = 0; sign_arr[i]; i++) {
 		if (sign_arr[i] == '+') {
-			converted_int_arr[i] = converted_int_arr[i] + converted_int_arr[i+1];
+			converted_int_arr[i] =
+			    converted_int_arr[i] + converted_int_arr[i + 1];
 			rearrange_char_arr(sign_arr, i);
-			rearrange_int_arr(converted_int_arr, i+1);
+			rearrange_int_arr(converted_int_arr, i + 1);
 			i--;
 		}
 		if (sign_arr[i] == '-') {
-			converted_int_arr[i] = converted_int_arr[i] - converted_int_arr[i+1];
+			converted_int_arr[i] =
+			    converted_int_arr[i] - converted_int_arr[i + 1];
 			rearrange_char_arr(sign_arr, i);
-			rearrange_int_arr(converted_int_arr, i+1);
+			rearrange_int_arr(converted_int_arr, i + 1);
 			i--;
 		}
 	}
@@ -75,26 +81,24 @@ void main()
 	}
 }
 
-
-
 void rearrange_int_arr(s16 *p, s16 i)
 {
 	s16 j;
-	for(j = i; j < MAX_DIGITS_TO_CALCULATE - 1; j++)
-		p[j] = p[j+1];
+	for (j = i; j < MAX_DIGITS_TO_CALCULATE - 1; j++)
+		p[j] = p[j + 1];
 }
 
 void rearrange_char_arr(s8 *p, s16 i)
 {
 	s16 j;
-	for(j = i; p[j]; j++)
-		p[j] = p[j+1];
+	for (j = i; p[j]; j++)
+		p[j] = p[j + 1];
 }
 
 s16 atoi(s8 *p)
 {
 	s16 num = 0, j;
-	for(j = 0; p[j]; j++) {
+	for (j = 0; p[j]; j++) {
 		num = (num * 10) + (p[j] - 48);
 	}
 	return num;
--- calculator_drivers.c
+++ calculator_drivers.c
@@ -1,17 +1,18 @@
-#include<reg51.h>
+#include <reg51.h>
 
 #include "calculator_app.h"
 
-code u8 tan[4][4] = {{'7','8','9','/'},
-					 {'4','5','6','*'},
-					 {'1','2','3','-'},
-					 {'C','0','=','+'}};
+code u8 tan[4][4] = {{'7', '8', '9', '/'},
+		     {'4', '5', '6', '*'},
+		     {'1', '2', '3', '-'},
+		     {'C', '0', '=', '+'}};
 
 u8 key()
 {
 	u8 row, col;
-	r0=r1=r2=r3=0;
-	while(c0 & c1 & c2 & c3);
+	r0 = r1 = r2 = r3 = 0;
+	while (c0 & c1 & c2 & c3)
+		;
 	r0 = 0;
 	r1 = 1;
 	r2 = 1;
@@ -24,7 +25,7 @@ u8 key()
 	r0 = 1;
 	r2 = 1;
 	r3 = 1;
-	if((c0 & c1 & c2 & c3) == 0) {
+	if ((c0 & c1 & c2 & c3) == 0) {
 		row = 1;
 		goto col;
 	}
@@ -32,7 +33,7 @@ u8 key()
 	r0 = 1;
 	r1 = 1;
 	r3 = 1;
-	if((c0 & c1 & c2 & c3) == 0) {
+	if ((c0 & c1 & c2 & c3) == 0) {
 		row = 2;
 		goto col;
 	}
@@ -40,21 +41,22 @@ u8 key()
 	r0 = 1;
 	r1 = 1;
 	r2 = 1;
-	if((c0 & c1 & c2 & c3) == 0) {
+	if ((c0 & c1 & c2 & c3) == 0) {
 		row = 3;
 		goto col;
 	}
 
-	col:
-	if(c0 == 0)
+col:
+	if (c0 == 0)
 		col = 3;
-	else if(c1 == 0)
+	else if (c1 == 0)
 		col = 2;
-	else if(c2 == 0)
+	else if (c2 == 0)
 		col = 1;
-	else if(c3 == 0)
+	else if (c3 == 0)
 		col = 0;
 
-	while((c0 & c1 & c2 & c3) == 0);
+	while ((c0 & c1 & c2 & c3) == 0)
+		;
 	return tan[row][col];
 }
